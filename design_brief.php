52bc6a2bc55863c67bf2506cb4ece26db263a07b
grace <gracetfg2@gmail.com>
2017-08-29 15:47:37 -0500

    to server

--- design_brief.php
+++ design_brief.php
@@ -1,4 +1,4 @@
 <?php 
-echo "You have been hired to create a poster for a charity jazz concert featuring the band \"Smooth Digital\", a group of four alumni from the School of Music at the University of Illinois. The concert will take place on October 12th from 6:00 PM - 9:00 PM on the Main Quad. Tickets are $10 per person, and food and drink will also be available for purchase. All proceeds will be used to support music programs at local elementary schools. Tickets can be purchased in the Illini Union Building in Room 208. "
+echo "You have been hired to create a poster for a charity jazz concert featuring the band \"Smooth Digital\", a group of four alumni from the School of Music at the University of Illinois. The concert will take place on October 12th from 6:00 PM - 9:00 PM on the Main Quad. Tickets are $10 per person, and food and drink will also be available for purchase. All proceeds will be used to support music programs at local elementary schools. Tickets can be purchased in the Illini Union Building in Room 208. The goal of the flyer is to encourage participation, be visually appealing, and convey the event details."
 
 ?>
