43b34a562983c93d3180214ec9e9ed7afce35f66
grace <gracetfg2@gmail.com>
2017-09-03 16:05:07 -0500

    merge

--- webpage-utility/db_config.php
+++ webpage-utility/db_config.php
@@ -1,14 +1,14 @@
 <?php
-	$DB_HOST = "localhost";
-	$DB = "crowdsight_interpretation";
-	$SQL_ACC="root";
-	$SQL_PWD = "1234567";
+	// $DB_HOST = "localhost";
+	// $DB = "crowdsight_interpretation";
+	// $SQL_ACC="root";
+	// $SQL_PWD = "1234567";
 
 
-  	// $DB_HOST = "webhost.engr.illinois.edu";
-   // 	$DB = "crowdsight_interpretation";
-   // 	$SQL_ACC = "crowdsig_1";
-   // 	$SQL_PWD = "bpteam!";
+  	$DB_HOST = "webhost.engr.illinois.edu";
+   	$DB = "crowdsight_interpretation";
+   	$SQL_ACC = "crowdsig_1";
+   	$SQL_PWD = "bpteam!";
 
   	// $DB_HOST = "webhost.engr.illinois.edu";
    // 	$DB = "crowdsight_interpretation";
