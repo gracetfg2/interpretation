e034e49ea669497c1a2e520a2809a7da91a09a28
grace <gracetfg2@gmail.com>
2017-09-12 02:17:37 -0500

    new pilot

--- task_explain_feedback.php
+++ task_explain_feedback.php
@@ -153,7 +153,9 @@ if ($stmt = mysqli_prepare($conn, "SELECT ProviderID From u_Provider WHERE IP = 
         <div id="p1" >
             <div class='row'>
                 <div class='col-md-6'>
-                <feedback><h4>Feedback #1 (Concept of the design): </h4>  Ok, so: "Smooth Digital! Cool, but, uh... Do we know them? Are they a band? What do they play?". Careful when the only giveaway that we are talking about a music show is the Sax player in the background, and right now he is more like a decoration than a proper element of the piece. Also you are taking away the main selling point of the event, we want to make profit by making a remark that this is a Charity event. Remember this is something that moves people to be there, that's why Charity drives always put that info upfront!</feedback>
+                <feedback><h4>Concept of the design: </h4>  Ok, so: "Smooth Digital! Cool, but, uh... Do we know them? Are they a band? What do they play?". Careful when the only giveaway that we are talking about a music show is the Sax player in the background, and right now he is more like a decoration than a proper element of the piece. Also you are taking away the main selling point of the event, we want to make profit by making a remark that this is a Charity event. Remember this is something that moves people to <br>be there, that's why Charity drives always put that info upfront!
+                Strengths: Strong and clean poster if Smooth Digital is a immediately recognizable brand.
+                <br>Weaknesses: Lacks key info: Charity event as a main information, and that we are talking about a Jazz band.</feedback>
                 </div>
                 <div class='col-md-6'>
                     <h5><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp  Please restate the meaning of feedback #1 using your own words:</h5><textarea name="f1" id="f1" monitorid="0" rows="10"></textarea>
@@ -166,9 +168,9 @@ if ($stmt = mysqli_prepare($conn, "SELECT ProviderID From u_Provider WHERE IP = 
         <div id="p2" style="display:none">
             <div class='row'>
             <div class='col-md-6'>
-                <feedback><h4>Feedback #2 (Layout and composition ): </h4>Great display on the information, it's the thing that pops out from it. But be careful not to block the view from the sax player, it's not entirely clear he's playing sax, and the cleanest way to save that is either taking the date and ticket price lower on the poster, right above the bottom info. Or lower the size of the sax player so he doesn't go behind the text.
-                Strengths: See that triangle shape the text makes? That helps with the readability of the poster. Gives the person a direction to their sight and the info presented. Remember that when adding the missing info.
-                Weaknesses: Sax player almost unrecognizable behind letters, best to keep him clear.</feedback>
+                <feedback><h4>Layout and composition: </h4>Great display on the information, it's the thing that pops out from it. But be careful not to block the view from the sax player, it's not entirely clear he's playing sax, and the cleanest way to save that is either taking the date and ticket price lower on the poster, right above the bottom info. Or lower the size of the sax player so he doesn't go behind the text.
+                <br>Strengths: See that triangle shape the text makes? That helps with the readability of the poster. Gives the person a direction to their sight and the info presented. Remember that when adding the missing info.
+                <br>Weaknesses: Sax player almost unrecognizable behind letters, best to keep him clear.</feedback>
                 </div>
                 <div class='col-md-6'>
                  <h5><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp Please restate the meaning of feedback #2 using your own words:</h5><textarea name="f2" id="f2" monitorid="1" rows="4"></textarea>     
@@ -180,9 +182,9 @@ if ($stmt = mysqli_prepare($conn, "SELECT ProviderID From u_Provider WHERE IP = 
             <div style="display:none" id="p3">
             <div class='row'>
                 <div class='col-md-6'>
-                <feedback><h4>Feedback #3 (Aesthetics): </h4>Besides having the sax player on full display, he can be brought up with a bit of color (as long as the attention doesn't shift to him, just enough to make him more recognizable), just stick to your color scheme so far. You can try just shadowing him with the blue like in "Smooth Digital", something discrete that makes him pop.
-                Strengths: Color Scheme. Use those yellows for relevant information. But see that the contrast in "Smooth Digital" works perfectly the way it is.
-                Weaknesses: Sax player almost blends with dark background. A small accent or change in color can bring him up without hoarding all the attention.</feedback>
+                <feedback><h4> Aesthetics of the design: </h4>Besides having the sax player on full display, he can be brought up with a bit of color (as long as the attention doesn't shift to him, just enough to make him more recognizable), just stick to your color scheme so far. You can try just shadowing him with the blue like in "Smooth Digital", something discrete that makes him pop.
+                <br> Strengths: Color Scheme. Use those yellows for relevant information. But see that the contrast in "Smooth Digital" works perfectly the way it is.
+                <br> Weaknesses: Sax player almost blends with dark background. A small accent or change in color can bring him up without hoarding all the attention.</feedback>
                 </div>
                 <div class='col-md-6'>
                 <h5><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp Please restate the meaning of feedback #3 using your own words:</h5><textarea name="f3" id="f3" monitorid="2" rows="4"></textarea>
