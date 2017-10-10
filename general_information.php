ccfa5821e2a7354c946272f5eeac410250e3d974
grace <gracetfg2@gmail.com>
2017-10-09 21:36:48 -0500

    launch

--- general_information.php
+++ general_information.php
@@ -1,8 +1,8 @@
 <?php 
 
 $admin_email= "design4uiuc@gmail.com";
-$first_deadline=" October 12nd at 11:59 pm (Central Time)";
-$second_deadline=" October 15th at 5 pm (Central Time)";
+$first_deadline=" October 20th at 11:59 pm (Central Time)";
+$second_deadline=" October 23th at 5 pm (Central Time)";
 $pay_deadline="October 31st";
 $contest_deadline="Octoberember 31st";
 $folder="interpretation";
