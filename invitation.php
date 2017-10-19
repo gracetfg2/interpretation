802e1675b15ec03b7761d8745fc04374ba8cbd27
grace <gracetfg2@gmail.com>
2017-10-19 15:16:36 -0500

    invitation

--- invitation.php
+++ invitation.php
@@ -24,7 +24,13 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+
+<style>
+.fig-cap{
+  text-align: center;
+}
+</style>
+</head>
 
   <body>
 
@@ -35,43 +41,29 @@
 
       <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron" style="margin-top:20px; padding-top:20px;background:white;margin-left:0px">
-<div><img src="school_logo.gif"></div>
+<div><img width='200px' src="school_logo.gif"></div>
 <br>
-        <h1 ><span style="font-size:42px;">Online Design Study</span></h1>
-
-        <h3>Be creative. Help us study online feedback exchange.</h3>
-        <p>We are investigating how a designer incorporates online feedback into a design process. In this study, you will create a flyer for a jazz concert, review feedback, perform an assigned activity, and revise the design. All designs and data collected in this study are for research purposes only.</p>
-             
-
-<hr>
-    <h3>Selection Criteria:  </h3> 
-      <p> No design background required. But to be eligible in the study: 
-              
-       <p> 
-       <ul style='font-size: 18px'> 
-        <li>You must be at least 18 years of age.</li>
-        <li>You must be willing to create an design and iterate on it.
-        </li>
-        <li>You must have some experience in designing any form of visual designs such as flyers, posters, logos, websites, or powerpoint slides.</li>
-<li>You must allow us to keep a copy of your design and feedback for research purposes</li>
-
-</ul>
-
-        </p>
-      
-<hr>         
-    <h3>Compensation and prizes:  </h3>
-      <p>​We will provide <strong>$25</strong> for full participation. The top 10 (out of
-120) designs will receive a bonus of $20 US dollars. All compensation will be distributed
-via Paypal, so you will need to provide a Paypal address if you want to receive the
-compensation.</p>
+  <h2 style="text-align:center;"><span>Welcome to the UIUC Online Design Study!</span></h2>
+    <p>You are invited to participate in an online study insestigating how a user incorporates feedback into a design process. In this study, you will practice creating a flyer for the American singer-songwriter, Taylor Swift. To help you improve your design, we invite two designers from Upwork to provide feedback. You can use their feedback to improve initial work. All designs and data collected in this study are for research purposes only. The time commitment will be 1 hour for the first stage and 1.5 hour for the second stage. All the activities are conducted online.</p>
 <hr>
 
-
+<h3>Compensation and prizes:  </h3>
+      <p>​We will provide <strong>$25</strong> for full participation. The top 10 designs will receive a bonus of <strong>$20 </strong>US dollars. All compensation will be distributed via Paypal, so you will need to provide a Paypal address if you want to receive the compensation.</p>
+<hr>
+  <h3>Selection Criteria:  </h3> 
+    <p> No design background required. But to be eligible:               
+    <p> 
+      <ul style='font-size: 18px'> 
+      <li>You must be at least 18 years of age.</li>
+      <li>You must be willing to create a design and iterate on it.</li>
+      <li>You must allow us to keep a copy of your design and feedback for research purposes</li>
+      </ul>
+  </p>       
+<hr>
 
 <h3>Procedure and Deadlines</h3>
-<p>​The study has two phases. In the first phase, you will need to submit your
-initial design addressing a given design brief. In the second phase, you will perform the activity assigned to you, revise your initial design and complete a survey. All the activities can be completed online.</p>
+<p>​The study has two phases. In the first phase, you will create and submit your
+initial design addressing our design brief. In the second phase, you will revise your design based on the given feedback, and complete a survey. All the activities can be completed online.</p>
 <p>
 <ul style='font-size: 18px'> 
 <li>Deadline for the first phase: <span style='color:red'> <?php echo $first_deadline; ?></span>. </li>
@@ -81,20 +73,31 @@ initial design addressing a given design brief. In the second phase, you will pe
 <p>Please check your schedule before signing up.</p>
 
 <hr>
+  
+<h3>About Us</h3>  
+    <p>We are computer science researchers passionate about building and studying technologies that influence design, creativity, and innovation processes. For more information, please visit our <a href="http://orchid.cs.illinois.edu/publications/index.html">group website
+    </a>.</p>
+    
+  <div class="row">
+  <p>
+  <figure class="col-md-3"><img src="grace.jpg" class="img-circle" width="200" height="200">  
+    <figcaption style='text-align: center;' class="fig-cap">
+      <strong>Grace Y. Yen </strong><br>PhD Candidate<br>Computer Science<br>UIUC</figcaption></figure>
+   <figure class="col-md-3"><img src="brian.jpg" class="img-circle" width="200" height="200">  <figcaption class="fig-cap"><strong>Dr. Brian Bailey</strong><br>Professor<br>Computer Science<br>UIUC</figcaption></figure>
+     <figure class="col-md-3"><img src="steven.jpg" class="img-circle" width="200" height="200">  <figcaption class="fig-cap"><strong>Dr. Steven Dow</strong><br>Professor<br>Computer Science<br>UCSD</figcaption></figure>
+     </p>
+  </div>
+<hr> 
 
-<h3>About Us</h3>
-<p>
-We are researchers from the Department of Computer Science at the University of Illinois at Urbana-Champaign. For more information, please visit our <a href="http://orchid.cs.illinois.edu/publications/index.html">group website</a>.
-</p>
-<hr>
+
 <h3>Contact Us </h3>
 <p>
 The study has been approved by the University of Illinois Institutional Review Board. If you have questions, please contact our research staff Grace Yen at <a href="mailto:design4uiuc@gmail.com">design4uiuc@gmail.com</a>  
 or click <strong>Sign Up</strong> to begin the study.
 </p>
 
-          <a class="btn btn-lg btn-primary" href="sign-up.php" role="button">Sign Up</a>
-        </p>
+<a class="btn btn-lg btn-primary" href="sign-up.php" role="button">Sign Up</a>
+  
 </div>
     </div> <!-- /container -->
 
---
+++ invitation3.php
@@ -0,0 +1,107 @@
+<?php 
+  include_once('general_information.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>UIUC Design Study</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+   
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container" style="padding-top:20px">
+
+
+
+
+      <!-- Main component for a primary marketing message or call to action -->
+<div class="jumbotron" style="margin-top:20px; padding-top:20px;background:white;margin-left:0px">
+<div><img src="school_logo.gif"></div>
+<br>
+        <h1 ><span style="font-size:42px;">Online Design Study</span></h1>
+
+        <h3>Be creative. Help us study online feedback exchange.</h3>
+        <p>We are investigating how a designer incorporates online feedback into a design process. In this study, you will create a flyer for a jazz concert, review feedback, perform an assigned activity, and revise the design. All designs and data collected in this study are for research purposes only.</p>
+             
+
+<hr>
+    <h3>Selection Criteria:  </h3> 
+      <p> No design background required. But to be eligible in the study: 
+              
+       <p> 
+       <ul style='font-size: 18px'> 
+        <li>You must be at least 18 years of age.</li>
+        <li>You must be willing to create an design and iterate on it.
+        </li>
+        <li>You must have some experience in designing any form of visual designs such as flyers, posters, logos, websites, or powerpoint slides.</li>
+<li>You must allow us to keep a copy of your design and feedback for research purposes</li>
+
+</ul>
+
+        </p>
+      
+<hr>         
+    <h3>Compensation and prizes:  </h3>
+      <p>​We will provide <strong>$25</strong> for full participation. The top 10 (out of
+120) designs will receive a bonus of $20 US dollars. All compensation will be distributed
+via Paypal, so you will need to provide a Paypal address if you want to receive the
+compensation.</p>
+<hr>
+
+
+
+<h3>Procedure and Deadlines</h3>
+<p>​The study has two phases. In the first phase, you will need to submit your
+initial design addressing a given design brief. In the second phase, you will perform the activity assigned to you, revise your initial design and complete a survey. All the activities can be completed online.</p>
+<p>
+<ul style='font-size: 18px'> 
+<li>Deadline for the first phase: <span style='color:red'> <?php echo $first_deadline; ?></span>. </li>
+<li>Deadline for the second phase: Three days after you complete the first stage of the study. </li>
+</ul>
+</p>
+<p>Please check your schedule before signing up.</p>
+
+<hr>
+
+<h3>About Us</h3>
+<p>
+We are researchers from the Department of Computer Science at the University of Illinois at Urbana-Champaign. For more information, please visit our <a href="http://orchid.cs.illinois.edu/publications/index.html">group website</a>.
+</p>
+<hr>
+<h3>Contact Us </h3>
+<p>
+The study has been approved by the University of Illinois Institutional Review Board. If you have questions, please contact our research staff Grace Yen at <a href="mailto:design4uiuc@gmail.com">design4uiuc@gmail.com</a>  
+or click <strong>Sign Up</strong> to begin the study.
+</p>
+
+          <a class="btn btn-lg btn-primary" href="sign-up.php" role="button">Sign Up</a>
+        </p>
+</div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+ 
+  </body>
+</html>
