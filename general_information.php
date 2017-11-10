624d1bcc04c158f323a09e9f7b75d6ebf8c4cb7d
grace <gracetfg2@gmail.com>
2017-11-10 15:48:09 -0600

    change deadline

--- general_information.php
+++ general_information.php
@@ -1,7 +1,7 @@
 <?php 
 
 $admin_email= "design4uiuc@gmail.com";
-$first_deadline=" November 12th at 11:59 pm (Central Time)";
+$first_deadline=" November 12th at 5 pm (Central Time)";
 $second_deadline=" Novenber 14th at 5 pm (Central Time)";
 $pay_deadline="Novenber 31st";
 $contest_deadline="November 31st";
