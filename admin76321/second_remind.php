a833812a44af51c0a7c0564c475124cf3b13665c
grace <gracetfg2@gmail.com>
2017-11-14 00:24:45 -0600

    change deadline

--- admin76321/second_remind.php
+++ admin76321/second_remind.php
@@ -22,7 +22,7 @@ $current_second_deadline='November 16th at 11:59 pm (Central Time)';
 
 
    $to      = $designer['email']; // Send email to our user
-    $subject = '[ Participant '.$designer['DesignerID'].'] Please complete the second phase of the study by November 15th. '; // Give the email a subject 
+    $subject = '*[ Participant '.$designer['DesignerID'].'] Complete the study by November 16th to receive the compensation'; // Give the email a subject 
     $message = '
     <html> 
     <body>
