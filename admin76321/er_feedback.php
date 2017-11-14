040f0bb983b7dd19e7fb8403fa4f5587e14b19fb
grace <gracetfg2@gmail.com>
2017-11-14 13:41:32 -0600

    raw data

--- admin76321/er_feedback.php
+++ admin76321/er_feedback.php
@@ -43,6 +43,17 @@ else {
 <html>
 <head>
  <title> Explain Reflect </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   
+    <script type="text/javascript" src="behavior_record_updated.js"></script>
+    <!---->
+    <title>Prototype</title>
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="../css/feedback.css">
 
 </head>
 <body>
@@ -93,7 +104,7 @@ else {
                 while ($myrow = $result->fetch_assoc()) {
                     $feedback[]=$myrow;
                 }  
-                 
+                $feedbackNum=1;
                  foreach ($feedback as $tmp)
                   {
                      $feedbackNum += 1;
