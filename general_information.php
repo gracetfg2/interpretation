cf24657d41ec07f5c0674fd86141131ebfec68a3
grace <gracetfg2@gmail.com>
2017-11-19 10:06:12 -0600

    date

--- general_information.php
+++ general_information.php
@@ -1,8 +1,8 @@
 <?php 
 
 $admin_email= "design4uiuc@gmail.com";
-$first_deadline=" November 12th at 5 pm (Central Time)";
-$second_deadline=" Novenber 14th at 5 pm (Central Time)";
+$first_deadline=" November 28th at 5 pm (Central Time)";
+$second_deadline=" Novenber 28th at 5 pm (Central Time)";
 $pay_deadline="Novenber 31st";
 $contest_deadline="November 31st";
 $folder="interpretation";
