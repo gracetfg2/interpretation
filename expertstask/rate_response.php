329d594329756ddb0a31f5517327f7fd10507280
grace <gracetfg2@gmail.com>
2018-01-26 14:33:13 -0600

    rate response

--- expertstask/rate_response.php
+++ expertstask/rate_response.php
@@ -25,15 +25,15 @@
  
 <div class="container"> 
  <div class='well' style='padding: 20px'>  
-    Below is a list of feedback collected for our designers. The designers were asked to rewrite the content of the feedback using their own words, and the response should cover all the content and keep the feedback's original meaning. 
+    Below is a list of feedback collected for our designers. The designers were asked to restate the content of the feedback using their own words, and the response should cover all the content and keep the feedback's original meaning. 
     <br><br>
     Your task is to rate to what degree do you think each response covers the meaning of its original feedback.
     <br><br>
  
     - 0%  : The response was not restating the meaning of the feedback. #124 <br>
-    - 25% : The response addressed some points in the feedback, but missed a majority of the content in the feedback. See #1<br>
-    - 50% : The response covered about a half of the critical points in the feedback. #10<br>
-    - 75% : The response addressed a majority but not all of the critical points in the feedback. <br>
+    - 25% : The response restated some points in the feedback, but missed a majority of the content in the feedback. See #1<br>
+    - 50% : The response restated about a half of the critical points in the feedback. #10<br>
+    - 75% : The response restated a majority but not all of the critical points in the feedback. <br>
     - 100% : The response restated and covered all the critical points in the feedback. See #3 and #5.<br>
     
 
