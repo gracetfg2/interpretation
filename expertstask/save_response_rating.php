3a71a0c4ea07493c7b46e9d3169a2ba6b984d45b
Yu-Chun Grace <gracetfg2@gmail.com>
2018-02-01 16:48:08 -0600

    debug

--- expertstask/save_response_rating.php
+++ expertstask/save_response_rating.php
@@ -5,6 +5,26 @@ $action= $_POST['action'];
 $provider=  $_POST['provider'];
 $rating=  $_POST['rating'];
 
-echo 'feedbackid'.$_POST['feedbackid'].'action'.$action.'provider'.$provider;
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/interpretation/webpage-utility/db_utility.php');
+$conn = connect_to_db();
+
+if($action=='update_rating')
+{
+
+if (!($stmt = mysqli_prepare($conn, "INSERT INTO ResponseRating (raterID, f_FeedbackID, rating) VALUES (?, ?, ?)
+
+  ON DUPLICATE KEY UPDATE
+  rating = VALUES(rating)"))) {
+    echo "SendData Global prepare failed: (" . $conn->errno . ") " . $conn->error;
+  }
+
+$stmt->bind_param("sii", $provider, $feedback_id, $rating);
+$stmt->execute();
+mysqli_stmt_close($stmt);
+
+}else{
+  echo "something went wrong";
+}
 
 ?>  
