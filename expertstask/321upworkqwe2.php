ab3a66b7e742d4fa1e4295014b78305531a520d7
Yu-Chun Grace <gracetfg2@users.noreply.github.com>
2018-02-02 10:15:40 -0600

    rate_Design done

--- expertstask/321upworkqwe2.php
+++ expertstask/321upworkqwe2.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
 
-include_once($_SERVER['DOCUMENT_ROOT'].'/reflection/webpage-utility/db_utility.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/interpretation/webpage-utility/db_utility.php');
 $conn = connect_to_db();
 
-if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Project` WHERE `total_version`=2 AND `ok`=1 ORDER BY ProjectID ASC")) {
+if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Project` WHERE `total_version`=2 AND `ok`=1 AND `f_DesignerId`>20 ORDER BY ProjectID ASC")) {
 	    		mysqli_stmt_execute($stmt2);
 	    		$result = $stmt2->get_result();
 	    		while ($myrow = $result->fetch_assoc()) {
 	    			$projects[]=$myrow;
 	    			
-	    		}  
-	    		
+	    		}  	    		
 	mysqli_stmt_close($stmt2);	
 }
 else {
@@ -21,8 +20,25 @@ else {
 	die();
 }
 
+foreach($projects as $value)
+{
+			$project_id=$value['ProjectID'];
 
+			//Get Designs 
+			if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Design` WHERE `f_ProjectID`=? ")) {
+			      mysqli_stmt_bind_param($stmt2, "i", $project_id);
+			      mysqli_stmt_execute($stmt2);
+			      $result = $stmt2->get_result();
+			      while ($myrow = $result->fetch_assoc()) {
+			          $designs[]=$myrow;
 
+			      }
+			}
+}
+mt_srand('321');
+$order = array_map(create_function('$val', 'return mt_rand();'), range(1, count($designs)));
+array_multisort($order, $designs);
+//shuffle($designs);
 
 ?>
 
@@ -30,12 +46,12 @@ else {
 <!DOCTYPE html>
 <html>
 <head>
-	<script src="/reflection/js/jquery-1.11.3.min.js"></script>
+	<script src="/interpretation/js/jquery-1.11.3.min.js"></script>
 	<?php include('../webpage-utility/ele_header.php'); ?>
 	<title> Review My Design </title>
 
 
-	<link rel="stylesheet" type="text/css" href="/reflection/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/interpretation/dist/css/bootstrap.min.css">
 <style>
 .pagination  li{   
   	cursor: pointer;
@@ -72,92 +88,43 @@ else {
 
   <?php 
 		$initial_msg=0;
-		foreach($projects as $value)
+		foreach($designs as $design)
 		{
-			$project_id=$value['ProjectID'];
+			$design_id=$design['DesignID'];		
+			$project_id=$design['f_ProjectID'];	
+			echo "<div id='p".$design_id."' class='pagecontent' style='display:none;'>";
 			
-		
-			echo "<div id='p".$value['ProjectID']."' class='pagecontent' style='display:none;'>";
-			
-			//Get current rating
-			if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Project` WHERE `ProjectID`=? ")) {
-				mysqli_stmt_bind_param($stmt2, "i", $project_id);
-				mysqli_stmt_execute($stmt2);
-				$result = $stmt2->get_result();
-				while ($current_project = $result->fetch_assoc()) {
-					global $id;
-					global $current_better;
-					global $current_doc;
-					$current_better= $current_project['better_rate2'];
-					$current_doc= $current_project['degreeOfChange_rate2'];
-					$id=$current_project['ProjectID'];
-				}
-
-   		 	}
 
 
 			//Get Designs 
-			if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Design` WHERE `f_ProjectID`=? ")) {
-			      mysqli_stmt_bind_param($stmt2, "i", $project_id);
-			      mysqli_stmt_execute($stmt2);
-			      $result = $stmt2->get_result();
-			      while ($myrow = $result->fetch_assoc()) {
-			        switch($myrow['version']){
-			          case 1: $initial=$myrow;
-			          case 2: $revised=$myrow;
-			        }
+			if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Evaluate` WHERE `f_ProjectID`=? ")) {
+			    
+			    mysqli_stmt_bind_param($stmt2, "i", $project_id);
+			    mysqli_stmt_execute($stmt2);
+			    $result = $stmt2->get_result();
+			    $record = $result->fetch_assoc();
+ 
+				global $rate;
 
-			      }
+				switch($design['version']){
+
+					case 1: $rate=$record['initial_rate1'];break;
+					case 2: $rate=$record['revised_rate1'];break;
+				  
+				} 
 			}
 
-			//Randomize the design order
-			$left=$initial;
-			$right=$revised;
-			$tmp = $right;
-			$value=rand()% 2;
-
-		    if ($value == 0) {
-		        $right = $left;
-		        $left = $tmp;
-		    }
 		    //Display
 		   
 		    echo "<div style='padding-top:50px'></div>";
-		    echo "<table style='text-align:center;'>
-		       
-
-		          <tr>
-		            <td><img class='left' width=400px height=600px style='border: 1px solid #A4A4A4;' src='../design/".$left['file']."'></td>
-		            <td width=40%></td>
-		            <td><img class='right' width=400px height=600px style='border: 1px solid #A4A4A4;' src='../design/".$right['file']."'></td>
-		          </tr>
-		         
-		        </table>";
-
-		 
-		    echo "<form style='padding-top:50px'>";
-		  
-		    echo "<h4>1. Please select which design you believe more effectively achieves the design goals (the placement is randomized):</h4>
-		  	 	<div class='form-group'>
-				<label class='radio-inline'>
-		  	 	&nbsp&nbsp&nbsp<input type='radio' name='project".$id."' value='".$left['DesignID']."' onclick='save(".$id.")'"; 
-		              if($current_better==$left['DesignID']) {echo 'checked';} echo "> The design on the left.
-		        </label>
-		        <label class='radio-inline'>
-		      	<input type='radio' name='project".$id."' value='".$right['DesignID']."' onclick='save(".$id.")'"; 
-		              if($current_better==$right['DesignID']) {echo 'checked';}echo "> The design on the right.
-		        </label>
-		        </div>";
-
-
-		    echo "<hr><div style='padding-top:20px'></div>";
-		    echo "<h4>2. Please rate the degree of difference between the two designs:</h4>
-		    <div class='sub_frame'> 
-		    
-		   <table>
- 				<tr>
- 				<td>
-	 				<table style='width:600px;text-align:center;' border='0' cellpadding='5' cellspacing='0'>
+		    echo "<div class='row'>
+		    		       
+		        <img width=450px style='border: 1px solid #A4A4A4;float:left;cursor: pointer;' src='../design/".$design['file']."' onclick=\"viewPic('".$design['mid']."')\";>
+					<div class='col-md-6' >
+					<div style='padding-left:20px'>
+						<h4>  Please rate the quality of the design based on the goals stated in the <a href='design_brief.php' target='_blank'>design brief</a>. </h4>
+					</div>
+					<table style='width:600px;text-align:center;' border='0' cellpadding='5' cellspacing='0'>
 						<tr aria-hidden='true'>
 							<td  class='radio-label'></td>
 							<td><label class='radio-cell'>1</label></td> 
@@ -171,23 +138,22 @@ else {
 						</tr>
 				
 						<tr>
-							<td class='radio-label' width='150px'><strong>Minor difference</strong></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."1'  value='1' "; if ($current_doc==1){echo "checked ";} echo "onclick='rate(".$id.",1);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."2'  value='2' "; if ($current_doc==2){echo "checked ";} echo "onclick='rate(".$id.",2);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."3'  value='3' "; if ($current_doc==3){echo "checked ";} echo "onclick='rate(".$id.",3);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."4'  value='4' "; if ($current_doc==4){echo "checked ";} echo "onclick='rate(".$id.",4);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."5'  value='5' "; if ($current_doc==5){echo "checked ";} echo "onclick='rate(".$id.",5);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."6'  value='6' "; if ($current_doc==6){echo "checked ";} echo "onclick='rate(".$id.",6);'></td>
-							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$id."' id='".$id."7'  value='7' "; if ($current_doc==7){echo "checked ";} echo "onclick='rate(".$id.",7);'></td>
-							<td class='radio-label' width='200px'><strong>Significant difference</strong></td>		
+							<td class='radio-label' width='100px'><strong>Very Low</strong></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."1'  value='1' "; if ($rate==1){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",1);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."2'  value='2' "; if ($rate==2){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",2);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."3'  value='3' "; if ($rate==3){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",3);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."4'  value='4' "; if ($rate==4){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",4);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."5'  value='5' "; if ($rate==5){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",5);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."6'  value='6' "; if ($rate==6){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",6);'></td>
+							<td class='radio-cell'><input type='radio' class='radio-inline' name='doc".$design['DesignID']."' id='".$design['DesignID']."7'  value='7' "; if ($rate==7){echo "checked ";} echo "onclick='rate(".$design['DesignID'].",".$design['f_ProjectID'].",".$design['version'].",".$design['f_DesignerID'].",7);'></td>
+							<td class='radio-label' width='100px'><strong>Very High</strong></td>		
 						</tr>
+						</table>
+				</div>
 
-					</table>
-				</td>
-				</tr>
-			</table>";
+			</div>";
 
-			echo "</div></div>";
+			echo "</div>";
 
 
 						
@@ -209,17 +175,35 @@ else {
 
      <?php 
 		$index=1;
-		foreach($projects as $value)
+		
+		foreach($designs as $value)
 		{
-			$current_class='indicator incomplete';
+			$project_id=$value['f_ProjectID'];
+			if ($stmt2 = mysqli_prepare($conn, "SELECT * FROM `Evaluate` WHERE `f_ProjectID`=? ")) {
+			    
+			    mysqli_stmt_bind_param($stmt2, "i", $project_id);
+			    mysqli_stmt_execute($stmt2);
+			    $result = $stmt2->get_result();
+			    $record = $result->fetch_assoc();
+ 
+				global $rate;
 
-			//Both not selected
-			if( !$value['better_rate2'] && !$value['degreeOfChange_rate2']) $current_class='indicator';
-			//Both selected
-			if( $value['better_rate2'] && $value['degreeOfChange_rate2'])$current_class='indicator finish';
+				switch($value['version']){
 
-			echo " <li class='".$current_class."' id='li".$value['ProjectID']."' name='li".$value['ProjectID']."'><a onclick='showUI(".$value['ProjectID'].")';>".$index."</a></li>";
-			$index++;
+					case 1: $rate=$record['initial_rate1'];break;
+					case 2: $rate=$record['revised_rate1'];break;
+				  
+				} 
+				
+				$current_class='indicator';
+				if($rate) $current_class='indicator finish';
+		
+	
+				echo " <li class='".$current_class."' id='li".$value['DesignID']."' name='li".$value['DesignID']."'><a onclick='showUI(".$value['DesignID'].")';>".$index."</a></li>";
+				$index++;
+	
+			}
+			
 		}
 	?>
 
@@ -242,9 +226,17 @@ else {
 $(document).ready(function() {
   // var task_params = document.getElementById("survey").href.substring(56).toString();
   // (new Image).src = 'http://128.174.241.28:8080/?' + task_params;
-	showUI(3);
+	$('.pagecontent:first').show();
+	$('li:first').addClass('active');
+
 });
 
+function viewPic(mid) {
+
+window.open("../viewpic.php?image="+mid);
+
+//viewwin = window.open(imgsrc.src,'viewwin', 'width=1000,height=auto'); 
+}
 
 	function showUI(_id){
 		$('#check-result').html();  
@@ -257,74 +249,26 @@ $(document).ready(function() {
     }
 
 
-    function save(_idx){
-    	$.ajax({
-        	type: "POST",
-            url:'save_compare.php',
-            data: {projectid: _idx ,action:'update_record',  selected: $('input[name=project'+_idx+']:checked').val() },
-            success: function (data) {
 
+
+
+    function rate(_idx, project_id, version, _designer, number){
+
+		$.ajax({
+        	type: "POST",
+            url:'321upworkqwe2_script.php',
+            data: { projectid: project_id , designid:_idx, version: version, designer:_designer, selected: number },
+            
+            success: function (data) {
             	
             	$('#li'+_idx).removeClass('active');
-            	$('#li'+_idx).removeClass('incomplete');
-            	$('#check-result').html('Selection saved!');   
-            	if ( $('input[name=doc'+_idx+']:checked').size() == 0 )
-				{
-					
-					$('#li'+_idx).addClass('incomplete');
-					$('#li'+_idx).addClass('active');
-				}
-				else
-            	{
-            		$('#li'+_idx).addClass('finish');
-            		//$('#li'+_idx).addClass('active');
-            		if(!$('#li'+_idx).is(':last-child'))
-            		{
-            			$('#li'+_idx).next().addClass('active');
-	            		$('#check-result').html();  
-						$('.pagecontent').hide();
-						$('#p'+_idx).next().show();
+   
+            	$('#check-result').html('Rating saved!');  
 
-            		}
-            		else
-            		{
-            			$('#li'+_idx).addClass('active');
-            			$('#check-result').html('Selection saved! This is the last project!');
-            		}
-            		
-            	}
-            	
-            	     	
-        	},
-            error: function () {
-            }
-        });
-    }
-
-
-
-    function rate(_idx, number){
-	
-    	$.ajax({
-        	type: "POST",
-            url:'save_compare.php',
-            data: {projectid: _idx ,action:'update_doc',  selected: number },
-            success: function (data) {
-
-            	$('#li'+_idx).removeClass('active');
-            	$('#li'+_idx).removeClass('incomplete');    
-            	$('#check-result').html('Degree of differences for the project is saved!');         
-            	if ($('input[name=project'+_idx+']:checked').size() == 0 )
+            	if ($('input[name=doc'+_idx+']:checked').size() > 0 )
 				{				
-					$('#li'+_idx).addClass('incomplete');
-					$('#li'+_idx).addClass('active');
-
-				}
-				else
-            	{
-            		$('#li'+_idx).addClass('finish');
-            		//$('#li'+_idx).addClass('active');
-            		if(!$('#li'+_idx).is(':last-child'))
+					$('#li'+_idx).addClass('finish');
+					if(!$('#li'+_idx).is(':last-child'))
             		{
             			$('#li'+_idx).next().addClass('active');
 	            		$('#check-result').html();  
@@ -337,8 +281,9 @@ $(document).ready(function() {
             			$('#li'+_idx).addClass('active');
             			$('#check-result').html('Selection saved! This is the last project!');
             		}
-            		
-            	}
+
+				}
+				
             	      	
         	},
             error: function () {
@@ -346,6 +291,7 @@ $(document).ready(function() {
 
 	
 		});
+		
 	}
 
 
