0e1e5ea60a9be2fee1a842320578224f15e84202
Yu-Chun Grace <gracetfg2@gmail.com>
2018-02-02 13:14:09 -0600

    update page

--- expertstask/design_brief.php
+++ expertstask/design_brief.php
@@ -1,1 +1,127 @@
-You have been hired to create a poster for a charity concert featuring Taylor Swift, one of the most famous American singer-songwriter. The concert will take place on November 29th from 6:00 PM - 9:00 PM at Krannert Center at University of Illinois at Urbana-Champaign. Tickets are $40 per person, and food and drink will also be available for purchase. All proceeds will be used to support music programs at local elementary schools. Tickets can be purchased in the Illini Union Building in Room 208. The goal of the flyer is to encourage participation, be visually appealing, and convey the event details.
+<?php 
+	
+	session_start();	
+
+	include($_SERVER['DOCUMENT_ROOT'].'/interpretation/general_information.php');
+
+ ?>
+<html lang="en">
+<head>
+    
+ 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+ 	 
+ 	<?php include('../webpage-utility/ele_header.php');?>
+ 	 <title>Home </title>
+    <!-- Custom styles for this template -->
+    <style>
+em{
+	color:red;
+}
+.btn-save {
+  background: #1a6b15;
+  background-image: -webkit-linear-gradient(top, #1a6b15, #115724);
+  background-image: -moz-linear-gradient(top, #1a6b15, #115724);
+  background-image: -ms-linear-gradient(top, #1a6b15, #115724);
+  background-image: -o-linear-gradient(top, #1a6b15, #115724);
+  background-image: linear-gradient(to bottom, #1a6b15, #115724);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  -webkit-box-shadow: 0px 1px 3px #666666;
+  -moz-box-shadow: 0px 1px 3px #666666;
+  box-shadow: 0px 1px 3px #666666;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 16px;
+  padding: 10px 15px 10px 15px;
+  text-decoration: none;
+}
+
+.btn-save:hover {
+  background: #063806;
+  background-image: -webkit-linear-gradient(top, #063806, #1a4223);
+  background-image: -moz-linear-gradient(top, #063806, #1a4223);
+  background-image: -ms-linear-gradient(top, #063806, #1a4223);
+  background-image: -o-linear-gradient(top, #063806, #1a4223);
+  background-image: linear-gradient(to bottom, #063806, #1a4223);
+  text-decoration: none;
+  color: #ffffff;
+}
+.btn-submit {
+  background: #e0eddf;
+  background-image: -webkit-linear-gradient(top, #e0eddf, #c5d9cb);
+  background-image: -moz-linear-gradient(top, #e0eddf, #c5d9cb);
+  background-image: -ms-linear-gradient(top, #e0eddf, #c5d9cb);
+  background-image: -o-linear-gradient(top, #e0eddf, #c5d9cb);
+  background-image: linear-gradient(to bottom, #e0eddf, #c5d9cb);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  -webkit-box-shadow: 0px 1px 3px #666666;
+  -moz-box-shadow: 0px 1px 3px #666666;
+  box-shadow: 0px 1px 3px #666666;
+  font-family: Arial;
+  color: #6d756e;
+  font-size: 16px;
+  padding: 10px 15px 10px 15px;
+  text-decoration: none;
+}
+
+.btn-submit:hover {
+  background: #d2dbd2;
+  background-image: -webkit-linear-gradient(top, #d2dbd2, #d9e0da);
+  background-image: -moz-linear-gradient(top, #d2dbd2, #d9e0da);
+  background-image: -ms-linear-gradient(top, #d2dbd2, #d9e0da);
+  background-image: -o-linear-gradient(top, #d2dbd2, #d9e0da);
+  background-image: linear-gradient(to bottom, #d2dbd2, #d9e0da);
+  text-decoration: none;
+    color: #6d756e;
+}
+.statement{
+	font-size: 16px;
+}
+.title{
+	color:#8A0808;
+}
+
+</style>
+ </head>
+
+ <body>
+ <?php include($_SERVER['DOCUMENT_ROOT'].'/interpretation/webpage-utility/ele_nav.php');?>
+
+
+
+
+<div class="main-section">
+	<div class="container ">
+
+	
+    <h4 class="title">Design Brief</h4>
+    <span class="statement" style="text-align: justify;">
+			    <ol>
+			    <?php include($_SERVER['DOCUMENT_ROOT'].'/interpretation/design_brief.php'); ?>
+
+				</ol>
+	</span>
+
+	<h4 class="title">Rules / Requirements</h4>
+    <span class="statement">
+ <ol>
+ <li>You should use your favorite software to design the flyer. No paper sketch allowed.</li>
+ <li>The flyer size should be 8.5" x 11" (US Letter size) and in portrait orientation.</li>
+ <li>The flyer must be created from scratch. No templates allowed.</li>
+ <li>You may use images from the public domain, but not profanity, obscenity, or nudity.</li>
+ </ol>
+
+ </span>
+
+
+  </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
