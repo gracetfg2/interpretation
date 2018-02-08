5d10d761d485f102283caa474a217ddf59afbc40
Yu-Chun Grace <gracetfg2@users.noreply.github.com>
2018-02-07 19:06:32 -0600

    debug

--- expertstask/degree_of_change2.php
+++ expertstask/degree_of_change2.php
@@ -142,9 +142,9 @@ else {
 			     	<h4>Please review the two designs and answer four questions:</h4>
 			     	 
 			     	 <table>
-			     	 <td><img class='left' width='40%'  style='border: 1px solid #A4A4A4;' src='../design/".$left['file']."'></td>		            	
+			     	 <td width='40%'><img class='left' style='border: 1px solid #A4A4A4;' src='../design/".$left['file']."'></td>		            	
 				           <td width='20%'></td>
-					 <td><img class='right' width='40%'  style='border: 1px solid #A4A4A4;' src='../design/".$right['file']."'></td>
+					 <td width='40%'><img class='right'  style='border: 1px solid #A4A4A4;' src='../design/".$right['file']."'></td>
 					 </table>
            		</div>
 
